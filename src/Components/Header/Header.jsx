8b25910f4e3a68416a073164f02c7af7713e78c0
munaciella <franes88@gmail.com>
2024-02-08 00:03:58 +0000

    add background to index.css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,14 +1,5 @@
 import "./Header.css"
 
-// const Header = () => {
-//     return (
-//         <header>
-//             <h1>Munaciella news</h1>
-//         </header>
-//     )
-// }
-
-// export default Header
 const Header = () => {
     return (
         <header>
